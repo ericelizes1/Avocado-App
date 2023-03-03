f1a26ea88d66b3969c369fad6f38bebadb1145af
Eric Elizes <84732908+ericelizes1@users.noreply.github.com>
2023-03-03 16:12:22 -0500

    Initial Bottom Tab Navigator Pages

--- avocado_src/App.js
+++ avocado_src/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+
+import HomeScreen from './App/HomeScreen';
+import DiscoverScreen from './App/DiscoverScreen';
+import NotificationsScreen from './App/NotificationsScreen';
+import ProfileScreen from './App/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Discover" component={DiscoverScreen} />
+        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
