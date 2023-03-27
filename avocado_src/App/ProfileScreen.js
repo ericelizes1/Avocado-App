c69e90bb7b1c5bbc03257387924dbacc737a401f
smichalski882 <sgm78@case.edu>
2023-03-27 17:58:21 -0400

    Added username and user bio to profile

--- avocado_src/App/ProfileScreen.js
+++ avocado_src/App/ProfileScreen.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function ProfileScreen() {
+  const username = 'Username';
+
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <View style={styles.header}>
+        <Text style={styles.username}>{username}</Text>
+      </View>
+      <View style={styles.bioContainer}>
+        <Text style={styles.bioHeader}>Bio</Text>
+        <TextInput
+          style={styles.bioInput}
+          placeholder="Enter your bio here"
+          multiline={true}
+          numberOfLines={4}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +30,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    padding: 10,
+  },
+  username: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  bioContainer: {
+    marginTop: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bioHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  bioInput: {
+    height: 150,
+    width: '80%',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 10,
+    padding: 10,
   },
 });
